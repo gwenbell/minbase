05f5a795f1c6356a5c3e1e86060768277248d7c8
Ev Bogue <ev@evbogue.com>
2017-05-25 21:52:23 -0400

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# Minbase
+
+Minbase is a minimal-as-possible client and library for Secure Scuttlebutt [Secure Scuttlebutt](http://scuttlebot.io).
+
+## License
+
+MIT
+
+
+
+
+
---
+++ modules/avatar-name.js
@@ -0,0 +1,20 @@
+var signifier = require('../plugs').first(exports.signifier = [])
+var h = require('hyperscript')
+
+exports.needs = { signifier: 'first' }
+
+exports.gives = 'avatar_name'
+
+exports.create = function (api) {
+
+  return function name (id) {
+    var n = h('span', id.substring(0, 10))
+
+    api.signifier(id, function (_, names) {
+      if(names.length) n.textContent = names[0].name
+    })
+
+    return n
+  }
+}
+
