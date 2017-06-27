cdfeb077c2ec87b48a9dba358316a98a21af50c2
Ev Bogue <ev@evbogue.com>
2017-06-27 13:18:05 -0400

    define avatar images via manual blob field

--- modules/avatar-edit.js
+++ modules/avatar-edit.js
@@ -20,32 +20,16 @@ exports.needs = {
 
 exports.gives = 'avatar_edit'
 
-function crop (d, cb) {
-  var canvas = hypercrop(h('img', {src: d}))
-
-  return h('div.column.avatar_pic',
-    canvas,
-    canvas.selection,
-    h('div.row.avatar_pic__controls',
-      h('button', 'okay', {onclick: function () {
-        cb(null, canvas.selection.toDataURL())
-      }}),
-      h('button', 'cancel', {onclick: function () {
-        cb(new Error('canceled'))
-      }})
-    )
-  )
-}
-
 exports.create = function (api) {
   return function (id) {
     var img = visualize(new Buffer(id.substring(1), 'base64'), 256)
     img.classList.add('avatar--profile')
 
     var lb = hyperlightbox()
-    var name_input = h('input', {placeholder: 'rename'})
+    var name_input = h('input', {placeholder: 'Input new name'})
     var name = api.avatar_name(id)
-    var selected = null, selected_data = null
+
+    var img_input = h('input', {placeholder: 'New profile pic blob'})
 
     getAvatar({links: api.sbot_links}, self_id, id, function (err, avatar) {
       if (err) return console.error(err)
@@ -53,86 +37,31 @@ exports.create = function (api) {
         img.src = api.blob_url(avatar.image)
     })
 
-    var also_pictured = h('div.profile__alsopicturedas.wrap')
-
-    pull(
-      api.sbot_links({dest: id, rel: 'about', values: true}),
-      pull.map(function (e) {
-        return e.value.content.image
-      }),
-      pull.filter(function (e) {
-        return e && 'string' == typeof e.link
-      }),
-      pull.unique('link'),
-      pull.drain(function (image) {
-        also_pictured.appendChild(
-          h('a', {
-            href:'#', onclick: function (ev) {
-              ev.stopPropagation()
-              ev.preventDefault()
-              selected = image
-              img.src = api.blob_url(image.link || image)
-            }
-          },
-          h('img.avatar--thumbnail', {src: api.blob_url(image)})
-          )
-        )
-      })
-    )
-
     return h('div.row.profile',
       lb,
       img,
       h('div.column.profile__info',
         h('strong', name),
         name_input,
-        hyperfile.asDataURL(function (data) {
-          var el = crop(data, function (err, data) {
-            if(data) {
-              img.src = data
-              var _data = dataurl.parse(data)
-              pull(
-                pull.once(_data.data),
-                api.sbot_blobs_add(function (err, hash) {
-                  if(err) return alert(err.stack)
-                  selected = {
-                    link: hash,
-                    size: _data.data.length,
-                    type: _data.mimetype,
-                    width: 512,
-                    height: 512
-                  }
-                })
-              )
-            }
-            lb.close()
-          })
-          lb.show(el)
-        }),
-        h('button', 'update', {onclick: function () {
+        h('button', 'Preview', {onclick: function () {
           if(name_input.value) {
-            name.textContent = name_input.value
-          }
-          if(selected) {
-            api.message_confirm({
-              type: 'about',
-              about: id,
-              name: name_input.value || undefined,
-              image: selected
-            })
-          }
-          else if(name_input.value) {
             api.message_confirm({
               type: 'about',
               about: id,
               name: name_input.value || undefined
             })
           }
-          else {
-            alert('If you\'ve just uploaded an image, give it a second to reach the server. If you haven\'t selected an image or name, please do that first.')
-          }
         }}),
-      also_pictured
+        img_input,
+        h('button', 'Preview', {onclick: function () {
+          if(img_input.value) {
+            api.message_confirm({
+              type: 'about',
+              about: id,
+              image: img_input.value || undefined
+            })
+          }
+        }})
       )
     )
   }
