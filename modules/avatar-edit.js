afe292a5d760a1668865f256701d1d8af2f08b17
Ev Bogue <ev@evbogue.com>
2017-06-27 14:24:19 -0400

    throw alert if blob url is not a blob

--- modules/avatar-edit.js
+++ modules/avatar-edit.js
@@ -26,10 +26,10 @@ exports.create = function (api) {
     img.classList.add('avatar--profile')
 
     var lb = hyperlightbox()
-    var name_input = h('input', {placeholder: 'Input new name'})
+    var name_input = h('input', {placeholder: 'New name'})
     var name = api.avatar_name(id)
 
-    var img_input = h('input', {placeholder: 'New profile pic blob'})
+    var img_input = h('input', {placeholder: 'New profile pic blob url'})
 
     getAvatar({links: api.sbot_links}, self_id, id, function (err, avatar) {
       if (err) return console.error(err)
@@ -55,11 +55,13 @@ exports.create = function (api) {
         img_input,
         h('button', 'Preview', {onclick: function () {
           if(img_input.value) {
-            api.message_confirm({
-              type: 'about',
-              about: id,
-              image: img_input.value || undefined
-            })
+            if (ref.isBlobId(img_input.value)) {
+              api.message_confirm({
+                type: 'about',
+                about: id,
+                image: img_input.value || undefined
+              })
+            } else { alert('The link you uploaded is not a blob, please use a valid blob id. - Example: &G7v7pgTXYfr4bTF7FB/qLiScmFIIOccsTV3Pp6bURB0=.sha256. - To upload a blob: write a normal message, upload a file, and publish. Square photos are best for avatar images.')}
           }
         }})
       )
