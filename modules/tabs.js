d6745b1d69c58fc58e4b0758e25118240d90b48f
Gwen <gwen@gwenbell.com>
2017-07-05 21:13:17 -0400

    move key access to tabs

--- modules/tabs.js
+++ modules/tabs.js
@@ -82,9 +82,6 @@ exports.create = function (api) {
       h('div', 
         h('a', {href: '#' + id}, img)
       ),
-      h('p.edit', 
-        h('a', {innerHTML: '<a href="#'+ id +'">Edit your profile</a> <a href="#Key"><img src="' + api.emoji_url('key') + '" class="emoji" /></a>'})
-      ),
       h('div.header__tabs', tabs.firstChild), //tabs
       h('div.header__search', h('div', search), api.menu())
     ), tabs.firstChild)
@@ -95,7 +92,7 @@ exports.create = function (api) {
   //  catch (_) { }
 
     if(!saved || saved.length < 3)
-      saved = ['Public', 'Direct', 'Mentions']
+      saved = ['Public', 'Direct', 'Mentions', 'Key']
 
     saved.forEach(function (path) {
       var el = api.screen_view(path)
