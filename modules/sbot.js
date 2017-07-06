39e8ba4302a5ab19328ca9ffa671406fc9a454dd
Ev Bogue <ev@evbogue.com>
2017-07-06 10:57:22 -0400

    update modules/sbot.js to use ssb-client@4.5.0 - 1.4.9

--- modules/sbot.js
+++ modules/sbot.js
@@ -47,7 +47,11 @@ module.exports = {
     sbot_gossip_connect: true,
     sbot_progress: true,
     sbot_publish: true,
-    sbot_whoami: true
+    sbot_whoami: true,
+
+    sbot_stream: true,
+    sbot_friends_get: true,
+    sbot_signs_get: true
   },
 
   create: function (api) {
@@ -92,24 +96,8 @@ module.exports = {
 
     return {
       connection_status: connection_status,
-      sbot_blobs_add: rec.sink(function (cb) {
-        return pull(
-          Hash(function (err, id) {
-            if(err) return cb(err)
-            //completely UGLY hack to tell when the blob has been sucessfully written...
-            var start = Date.now(), n = 5
-            ;(function next () {
-              setTimeout(function () {
-                sbot.blobs.has(id, function (err, has) {
-                  if(has) return cb(null, id)
-                  if(n--) next()
-                  else cb(new Error('write failed'))
-                })
-              }, Date.now() - start)
-            })()
-          }),
-          sbot.blobs.add()
-        )
+      sbot_blobs_add: rec.sink(function (cb)  { 
+        return sbot.blobs.add(cb) 
       }),
       sbot_links: rec.source(function (query) {
         return sbot.links(query)
@@ -172,10 +160,19 @@ module.exports = {
       }),
       sbot_whoami: rec.async(function (cb) {
         sbot.whoami(cb)
-      })
+      }),
+
+      sbot_stream: rec.source(function (opts) {
+        return sbot.stream(opts)
+      }),
+      sbot_friends_get: rec.async(function (opts, cb) {
+        return sbot.friends.get(opts, cb)
+      }),
+      sbot_signs_get: rec.async(function (opts, cb) {
+        return sbot.signs.get(opts, cb)
+      }),
+
     }
   }
 }
-
-
 
