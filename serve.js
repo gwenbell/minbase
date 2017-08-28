d697848e531e6619eb707f6374578ba7c8858b97
Ev Bogue <ev@evbogue.com>
2017-08-28 14:46:11 -0700

    make sure to pass opts.allowPrivate to party

--- serve.js
+++ serve.js
@@ -3,13 +3,15 @@ var serve = require('ecstatic');
 var client = require('ssb-client')
 var party = require('ssb-party')
 
-party(function (err, sbot) {
+opts = {"allowPrivate": true}
+
+party(opts, function (err, sbot) {
   http.createServer(
     serve({ root: __dirname + '/build/'})
-  ).listen(3013);
+  ).listen(3013)
   
   opts = {"modern": true}
-  
+ 
   client(function (err, sbot) {  
     if(err) throw err
     sbot.invite.create(opts, function (err, invite) {
